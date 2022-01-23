cf5ff86049214817f43c0181488f18c3cb13889b
stdj91 <stdj.1907@gmail.com>
2022-01-23 11:58:07 +0100

    Organizing Component

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,18 @@
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+
+    return(
+        <Card className="expense-item">
+            <ExpenseDate date={props.date}/>
+            <div  className="expense-item_description">
+                <h2>{props.title}</h2>
+            </div>
+            <div className="expense-item__price">{props.amount}</div>
+        </Card>
+    )
+}
+
+export default ExpenseItem;
