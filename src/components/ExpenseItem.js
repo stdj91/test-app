6e973979a38c21f6941b2ff319670493ffc56fd2
stdj91 <stdj.1907@gmail.com>
2022-01-23 10:38:57 +0100

    Splitting Components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,11 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
 
-   
-
     return(
         <div className="expense-item">
-            <div>{props.date.toISOString()}</div>
+            <ExpenseDate date={props.date}/>
             <div  className="expense-item_description">
                 <h2>{props.title}</h2>
             </div>
