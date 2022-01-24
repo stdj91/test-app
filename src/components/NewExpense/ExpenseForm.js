d0d9b018d9a89da93710da8f3ccb21f6de600aca
stdj91 <stdj.1907@gmail.com>
2022-01-24 11:42:19 +0100

    Clear input after addExpense

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -48,21 +48,24 @@ const ExpenseForm = () => {
         }
 
         console.log(expenseData)
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
     }
 
     return <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Title</label>
-                <input type="text" onChange={titleChangeHandler}/>
+                <input type="text" onChange={titleChangeHandler} value={enteredTitle}/>
             </div>
             <div className="new-expense__control">
                 <label>Amount</label>
-                <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
+                <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} value={enteredAmount}/>
             </div>
             <div className="new-expense__control">
                 <label>Date</label>
-                <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
+                <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} value={enteredDate}/>
             </div>
         </div>
         <div className="new-expense__actions">
