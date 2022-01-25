b83f971da509437ff5cb102417333e77f3c2260e
stdj91 <stdj.1907@gmail.com>
2022-01-25 15:40:23 +0100

    HOF Map added as element filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -16,26 +16,15 @@ const Expenses = (props) => {
       <div>
         <Card className="expenses">
         <ExpensesFilter onChangeFilter={filterChangeHandler} selected={filteredYear}/>
+        {props.items.map((expense) => {
+          return (
             <ExpenseItem 
-        title={props.items[0].title}  
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem 
-        title={props.items[1].title}  
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem 
-        title={props.items[2].title}  
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem 
-        title={props.items[3].title}  
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date} 
+            /> 
+          )
+        })}
         </Card>
       </div>
     )
